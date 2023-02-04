4d33ef199631e967c36d310d7bbdb2765673a3ae
Amy Detwiler <detwiler.amy@gmail.com>
2023-02-04 15:51:21 -0500

    initial setup

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-landing-background">
+    <div class="font-bold text-center text-7xl text-white">hello</div>
+    <NuxtImg src="/landing/with_fuzz_girls.png" />
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'landing-background': '#082407'
+      }
+    },
+  },
+  plugins: [],
+}
